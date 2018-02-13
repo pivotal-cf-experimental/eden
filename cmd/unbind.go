8f92ee53eaee375da8eab2b9e0370859845c0310
Henry Stanley <hstanley@pivotal.io>
2018-02-13 17:49:27 +0000

    Unbind now uses state from the service controller

--- cmd/unbind.go
+++ cmd/unbind.go
@@ -9,17 +9,15 @@ import (
 
 // UnbindOpts represents the 'unbind' command
 type UnbindOpts struct {
-  BindingID       string `short:"b" long:"bind" description:"Binding ID to destroy" required:"true"`
+	BindingID string `short:"b" long:"bind" description:"Binding ID to destroy" required:"true"`
 }
 
 // Execute is callback from go-flags.Commander interface
 func (c UnbindOpts) Execute(_ []string) (err error) {
-  instanceNameOrID := Opts.Instance.NameOrID
-  if instanceNameOrID == "" {
-    return fmt.Errorf("unbind command requires --instance [NAME|GUID], or $SB_INSTANCE")
-  }
-	instance := Opts.config().FindServiceInstance(instanceNameOrID)
-	// TODO: convert c.BindingID into ID if its a name
+	instanceNameOrID := Opts.Instance.NameOrID
+	if instanceNameOrID == "" {
+		return fmt.Errorf("unbind command requires --instance [NAME|GUID], or $SB_INSTANCE")
+	}
 
 	broker := apiclient.NewOpenServiceBroker(
 		Opts.Broker.URLOpt,
@@ -27,12 +25,16 @@ func (c UnbindOpts) Execute(_ []string) (err error) {
 		Opts.Broker.ClientSecretOpt,
 		Opts.Broker.APIVersion,
 	)
+
+	instance, err := broker.GetServiceInstance(instanceNameOrID)
+	if err != nil {
+		return errwrap.Wrapf("Failed to get service instance {{err}}", err)
+	}
 	err = broker.Unbind(instance.ServiceID, instance.PlanID, instance.ID, c.BindingID)
 	if err != nil {
 		return errwrap.Wrapf("Failed to unbind to service instance {{err}}", err)
 	}
-	Opts.config().UnbindServiceInstance(instance.ID, c.BindingID)
 
-  fmt.Println("Success")
+	fmt.Println("Success")
 	return
 }
